6ab55a4994f73853356b3b8a6d788150e4adb8f7
Darryl Hebbes <darryl.hebbes@gmail.com>
2021-05-28 10:28:54 +0200

    fix types regex
    Make sure type forms are only checked at column 0

--- pine-script-mode.el
+++ pine-script-mode.el
@@ -53,7 +53,7 @@
 (defvar pine-script-font-lock-keywords
   `(("$$(\\s *\\w+\\s *)" (0 font-lock-function-name-face t))
     ("$$\\[\\s *\\w+\\s *\\]" (0 font-lock-function-name-face t))
-    (,(regexp-opt '("float" "int" "bool" "string" "color") `words)
+    (,(concat "^" (regexp-opt '("float" "int" "bool" "string" "color") `words))
        (0 font-lock-type-face))   
     (,(regexp-opt '("accdist" "adjustment.dividends" "adjustment.none" "adjustment.splits"
                     "alert.freq_all" "alert.freq_once_per_bar" "alert.freq_once_per_bar_close"
